fd461c7cda920abb2e28f0e238a2e31d9ae54cc9
Sagar Khadka <khadkasagar109@gmail.com>
2024-04-05 23:29:33 +0545

    added cuberto reeler

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
 import HeroSlider from '@/components/HeroSlider'
+import Reeler from '@/components/Reeler'
 
 export default function Home() {
   return (
     <>
       <HeroSlider />
+      <Reeler />
     </>
   )
 }
---
+++ components/Reeler.tsx
@@ -0,0 +1,41 @@
+'use client'
+
+import Reeller from 'reeller'
+import gsap from 'gsap'
+
+Reeller.registerGSAP(gsap)
+
+const Reeler = () => {
+  const reeller = new Reeller({
+    container: '.my-reel',
+    wrapper: '.my-reel-wrap',
+    itemSelector: '.my-reel-item',
+    speed: 10
+  })
+
+  return (
+    <>
+      <div className='my-reel py-14'>
+        <div className='my-reel-wrap flex gap-8'>
+          <div className='my-reel-item text-semibold rounded-full bg-slate-200 px-8 py-3 text-xl'>
+            Hello
+          </div>
+          <div className='my-reel-item text-semibold rounded-full bg-slate-200 px-8 py-3 text-xl'>
+            Hello
+          </div>
+          <div className='my-reel-item text-semibold rounded-full bg-slate-200 px-8 py-3 text-xl'>
+            Hello
+          </div>
+          <div className='my-reel-item text-semibold rounded-full bg-slate-200 px-8 py-3 text-xl'>
+            Hello
+          </div>
+          <div className='my-reel-item text-semibold rounded-full bg-slate-200 px-8 py-3 text-xl'>
+            Hello
+          </div>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default Reeler
