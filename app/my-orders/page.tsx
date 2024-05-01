23d24a213cd94ef19bf269ad3b432882027e3fe9
Sagar Khadka <khadkasagar109@gmail.com>
2024-05-01 23:08:19 +0545

    order page

---
+++ app/my-orders/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <>
+      <section className='container'>
+        <h2 className='text-2xl'>My Orders</h2>
+      </section>
+    </>
+  )
+}
+
+export default page
---
+++ components/products/ProductDetailCard.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Card } from '../ui/card'
+
+const ProductDetailCard = () => {
+  return (
+    <>
+      <Card></Card>
+    </>
+  )
+}
+
+export default ProductDetailCard
